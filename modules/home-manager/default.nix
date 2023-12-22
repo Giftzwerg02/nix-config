2c10c52b09f73458a0330348519c32b8fcaa4a0f
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-22 19:58:57 +0100

    i3 hm config

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,5 +2,5 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
-  my-i3-config = import ./my-i3.nix;
+  # my-module = import ./my-module.nix;
 }
