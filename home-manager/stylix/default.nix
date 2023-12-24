8c5509bdc9fcf4784dce703735befe528b9c5ff0
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-24 01:22:42 +0100

    i3 hm config

--- home-manager/stylix/default.nix
+++ home-manager/stylix/default.nix
@@ -11,13 +11,15 @@ in
 
   config = lib.mkIf cfg.enable
     {
+
       stylix.image = pkgs.fetchurl {
         url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
         sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
       };
       stylix = {
         polarity = "dark";
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+        # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
         cursor = {
           name = "capitaine-cursors";
           package = pkgs.capitaine-cursors;
