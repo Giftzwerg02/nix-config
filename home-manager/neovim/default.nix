c76f774b70206d527acf4487559a3aa9c61b949d
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-25 22:35:19 +0100

    migration to nixvim

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -10,86 +10,92 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.neovim =
-      let
-        toLuaFile = file: "\n${builtins.readFile file}\n";
-      in
-      {
-        enable = true;
-        package = pkgs.neovim-nightly;
+    programs.nixvim = {
+      enable = true;
+    }
 
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
 
-        extraPackages = with pkgs; [
-          luajitPackages.lua-lsp
-          rnix-lsp
-
-          xclip
-          wl-clipboard
-        ];
-
-        plugins = with pkgs.vimPlugins; [
-          rust-tools-nvim
-          nvim-jdtls
-          {
-            type = "lua";
-            plugin = nvim-lspconfig;
-            config = ''
-              ${toLuaFile ./nvim/plugin/lsp.lua}
-            '';
-          }
-          lualine-nvim
-          neodev-nvim
-          vim-fugitive
-          vim-rhubarb
-          vim-sleuth
-          luasnip
-          friendly-snippets
-          cmp_luasnip
-          {
-            type = "lua";
-            plugin = nvim-cmp;
-            config = toLuaFile ./nvim/plugin/cmp.lua;
-          }
-          cmp-nvim-lsp
-          {
-            type = "lua";
-            plugin = which-key-nvim;
-            config = "require('which-key').setup()";
-          }
-          gitsigns-nvim
-          indent-blankline-nvim
-          comment-nvim
-          {
-            type = "lua";
-            plugin = telescope-nvim;
-            config = toLuaFile ./nvim/plugin/telescope.lua;
-          }
-          telescope-fzf-native-nvim
-          {
-            type = "lua";
-            plugin = (nvim-treesitter.withPlugins (p: [
-              p.tree-sitter-nix
-              p.tree-sitter-vim
-              p.tree-sitter-bash
-              p.tree-sitter-lua
-              p.tree-sitter-python
-              p.tree-sitter-json
-              p.tree-sitter-typescript
-              p.tree-sitter-tsx
-              p.tree-sitter-rust
-            ]));
-            config = toLuaFile ./nvim/plugin/treesitter.lua;
-          }
-          nvim-autopairs
-        ];
-
-        extraLuaConfig = ''
-          ${builtins.readFile ./nvim/options.lua}
-          ${builtins.readFile ./nvim/plugin/other.lua}
-        '';
-      };
+    # programs.neovim =
+    #   let
+    #     toLuaFile = file: "\n${builtins.readFile file}\n";
+    #   in
+    #   {
+    #     enable = true;
+    #     package = pkgs.neovim-nightly;
+    #
+    #     viAlias = true;
+    #     vimAlias = true;
+    #     vimdiffAlias = true;
+    #
+    #     extraPackages = with pkgs; [
+    #       luajitPackages.lua-lsp
+    #       rnix-lsp
+    #
+    #       xclip
+    #       wl-clipboard
+    #     ];
+    #
+    #     plugins = with pkgs.vimPlugins; [
+    #       rust-tools-nvim
+    #       nvim-jdtls
+    #       {
+    #         type = "lua";
+    #         plugin = nvim-lspconfig;
+    #         config = ''
+    #           ${toLuaFile ./nvim/plugin/lsp.lua}
+    #         '';
+    #       }
+    #       lualine-nvim
+    #       neodev-nvim
+    #       vim-fugitive
+    #       vim-rhubarb
+    #       vim-sleuth
+    #       luasnip
+    #       friendly-snippets
+    #       cmp_luasnip
+    #       {
+    #         type = "lua";
+    #         plugin = nvim-cmp;
+    #         config = toLuaFile ./nvim/plugin/cmp.lua;
+    #       }
+    #       cmp-nvim-lsp
+    #       {
+    #         type = "lua";
+    #         plugin = which-key-nvim;
+    #         config = "require('which-key').setup()";
+    #       }
+    #       gitsigns-nvim
+    #       indent-blankline-nvim
+    #       comment-nvim
+    #       {
+    #         type = "lua";
+    #         plugin = telescope-nvim;
+    #         config = toLuaFile ./nvim/plugin/telescope.lua;
+    #       }
+    #       telescope-fzf-native-nvim
+    #       {
+    #         type = "lua";
+    #         plugin = (nvim-treesitter.withPlugins (p: [
+    #           p.tree-sitter-nix
+    #           p.tree-sitter-vim
+    #           p.tree-sitter-bash
+    #           p.tree-sitter-lua
+    #           p.tree-sitter-python
+    #           p.tree-sitter-json
+    #           p.tree-sitter-typescript
+    #           p.tree-sitter-tsx
+    #           p.tree-sitter-rust
+    #           p.tree-sitter-latex
+    #         ]));
+    #         config = toLuaFile ./nvim/plugin/treesitter.lua;
+    #       }
+    #       nvim-autopairs
+    #     ];
+    #
+    #     extraLuaConfig = ''
+    #       ${builtins.readFile ./nvim/options.lua}
+    #       ${builtins.readFile ./nvim/plugin/other.lua}
+    #     '';
+    #   };
   };
 }
