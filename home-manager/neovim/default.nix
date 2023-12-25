eda702c84ccd79ab504278bef1a14cb241bc99fa
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-25 23:45:16 +0100

    migration to nixvim

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -80,13 +80,6 @@ in
           };
           capabilities = "require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())";
           onAttach = ''          
-            function(_, bufnr)
-              -- NOTE: Remember that lua is a real programming language, and as such it is possible
-              -- to define small helper and utility functions so you don't have to repeat yourself
-              -- many times.
-              --
-              -- In this case, we create a function that lets us more easily define mappings specific
-              -- for LSP related items. It sets the mode, buffer and description for us each time.
               local nmap = function(keys, func, desc)
                 if desc then
                   desc = 'LSP: ' .. desc
@@ -121,7 +114,6 @@ in
               vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
                 vim.lsp.buf.format()
               end, { desc = 'Format current buffer with LSP' })
-            end
           '';
           keymaps = { };
         };
