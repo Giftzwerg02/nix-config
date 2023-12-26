dc021db9930915042d5651b080187af4befefa2b
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-26 14:25:42 +0100

    migration to nixvim

--- home-manager/stylix/default.nix
+++ home-manager/stylix/default.nix
@@ -17,7 +17,7 @@ in
         sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
       };
       stylix = {
-        targets.nixvim.enable = true;
+        targets.nixvim.enable = false;
         polarity = "dark";
         base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
         cursor = {
