d46761f525d5b0c9bfb65f2b221f2f4e102e43c6
Giftzwerg02 <chessplayer1@gmx.at>
2023-12-27 23:10:46 +0100

    global.nix

--- nixos/global.nix
+++ nixos/global.nix
@@ -15,38 +15,8 @@ in {
     };
   };
   config = {
-    global-setup = lib.mkIf cfg.enable {
-      nixpkgs = {
-        overlays = [
-          outputs.overlays.additions
-          outputs.overlays.modifications
-          outputs.overlays.unstable-packages
-          inputs.neovim-nightly-overlay.overlays.default
-        ];
-        config = {
-          allowUnfree = true;
-        };
-      };
-
-      nix = {
-        # This will add each flake input as a registry
-        # To make nix3 commands consistent with your flake
-        registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-        # This will additionally add your inputs to the system's legacy channels
-        # Making legacy nix commands consistent as well, awesome!
-        nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-        settings = {
-          # Enable flakes and new 'nix' command
-          experimental-features = "nix-command flakes";
-          # Deduplicate and optimize nix store
-          auto-optimise-store = true;
-
-          # Force clean git-directory before rebuilding
-          allow-dirty = false;
-        };
-      };
+    global-setup = with config; lib.mkIf cfg.enable {
+      
 
       # Use latest Kernel
       boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
