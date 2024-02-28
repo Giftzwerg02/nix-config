c63fe51ecb2d66b431dff0fd97b9c4ff48e7268f
Giftzwerg02 <chessplayer1@gmx.at>
2024-02-28 19:16:55 +0100

    weasyprint

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -389,6 +389,22 @@ in {
           };
           onAttach = lspKeymapsOnAttach;
 		};
+
+		lint = {
+			enable = true;
+			lintersByFt = {
+				nix = ["statix"];
+				lua = ["selene"];
+				python = ["flake8"];
+				javascript = ["eslint_d"];
+				javascriptreact = ["eslint_d"];
+				typescript = ["eslint_d"];
+				typescriptreact = ["eslint_d"];
+				json = ["jsonlint"];
+				java = ["checkstyle"];
+			};
+		};		
+
       };
     };
   };
