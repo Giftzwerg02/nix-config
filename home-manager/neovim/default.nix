6fb7a89fc085fe7896bd5eabae20e3148c1b8f15
Giftzwerg02 <chessplayer1@gmx.at>
2024-03-08 22:06:52 +0100

    go templ

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -275,7 +275,7 @@ in {
 				"<C-p>" = /* lua */ "cmp.mapping.select_prev_item()";
 				"<C-d>" = /* lua */ "cmp.mapping.scroll_docs(-4)";
 				"<C-f>" = /* lua */ "cmp.mapping.scroll_docs(4)";
-				"<C-Space>" = /* lua */ "cmp.mapping.complete {}";
+				"<C-Space>" = /* lua */ "cmp.mapping.complete()";
 				"<CR>" = /* lua */ ''
 				  cmp.mapping.confirm {
 					behavior = cmp.ConfirmBehavior.Replace,
