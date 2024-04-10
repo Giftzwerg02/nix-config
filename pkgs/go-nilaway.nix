7d2defe36320d1753da6249872ac9f133adb590c
Giftzwerg02 <chessplayer1@gmx.at>
2024-04-10 19:28:35 +0200

    go-nilaway

--- pkgs/go-nilaway.nix
+++ pkgs/go-nilaway.nix
@@ -8,7 +8,7 @@ buildGoModule rec {
 	owner = "uber-go";
 	repo = "nilaway";
 	rev = "755a685ab68b85d9b36833b38972a559f217d396";
-	hash = "";
+	hash = "sha256-sDDBITrGD79pcbsrcrs6D8njBp4kuK1NkuBPwzIkaUE=";
   };
 
   vendorHash = "sha256-kbVjkWW5D8jp5QFYGiyRuGFArRsQukJIR8xwaUUIUBs=";
