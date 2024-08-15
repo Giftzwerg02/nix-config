8b835657f8652cd5cd4c8cdaa1fc169853388bda
Giftzwerg02 <chessplayer1@gmx.at>
2024-08-15 20:26:20 +0200

    candy-crusher

--- nixos/candy-crusher/configuration.nix
+++ nixos/candy-crusher/configuration.nix
@@ -120,7 +120,7 @@ in
 						# 	FOOBAR="sussy"; 
 						# 	#rconpwd="$(cat ${config.sops.secrets."minecraft-servers/vanilla-1/rcon-password".path})";
 						# };
-						environment = {
+						environment =  {
 							F = "sussy";
 						}; #.FOOBAR = "sussy";
 						serverProperties = {
