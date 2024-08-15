8d2555da7ce36b0c3f684f25eed365cd9a9ebc44
Giftzwerg02 <chessplayer1@gmx.at>
2024-08-15 16:31:23 +0200

    candy-crusher

--- nixos/candy-crusher/configuration.nix
+++ nixos/candy-crusher/configuration.nix
@@ -58,7 +58,7 @@ in
 		age.keyFile = "~/.config/sops/age/keys.txt";
 
 		secrets = {
-			"minecraft-servers/vanilla-1/rcon-password" = {
+			"minecraft-servers/vanilla-1/rcon" = {
 			};
 		};
 	};
@@ -91,16 +91,16 @@ in
 			];
 			nixpkgs.config.allowUnfree = true;
 
-			sops = {
-		defaultSopsFile = ./secrets/secrets.yaml;
-		defaultSopsFormat = "yaml";
-		age.keyFile = "~/.config/sops/age/keys.txt";
-
-		secrets = {
-			"minecraft-servers/vanilla-1/rcon-password" = {
-			};
-		};
-	};
+	# 		sops = {
+	# 	defaultSopsFile = ./secrets/secrets.yaml;
+	# 	defaultSopsFormat = "yaml";
+	# 	age.keyFile = "~/.config/sops/age/keys.txt";
+	#
+	# 	secrets = {
+	# 		"minecraft-servers/vanilla-1/rcon" = {
+	# 		};
+	# 	};
+	# };
 
 			services.minecraft-servers = {
 				enable = true;
