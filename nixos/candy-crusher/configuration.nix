728646063ec124642d0bd299909875944eeca5b2
Giftzwerg02 <chessplayer1@gmx.at>
2024-08-15 16:06:57 +0200

    candy-crusher

--- nixos/candy-crusher/configuration.nix
+++ nixos/candy-crusher/configuration.nix
@@ -1,4 +1,11 @@
-{ config, pkgs, inputs, ... }: 
+{ 
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: 
 
 let
 domain = "giftzwerg02.duckdns.org";
@@ -24,6 +31,26 @@ in
 		inputs.nix-minecraft.nixosModules.minecraft-servers
 		inputs.sops-nix.nixosModules.sops
 	];
+
+	nix = {
+		# This will add each flake input as a registry
+		# To make nix3 commands consistent with your flake
+		registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+		# This will additionally add your inputs to the system's legacy channels
+		# Making legacy nix commands consistent as well, awesome!
+		nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+		settings = {
+		  # Enable flakes and new 'nix' command
+		  experimental-features = "nix-command flakes";
+		  # Deduplicate and optimize nix store
+		  auto-optimise-store = true;
+
+		  # Force clean git-directory before rebuilding
+		  allow-dirty = false;
+		};
+	};
 
 	sops = {
 		defaultSopsFile = ./secrets/secrets.yaml;
