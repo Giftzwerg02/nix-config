422978f4ed358b31fbdf6b9aab29c34f363a453e
Giftzwerg02 <chessplayer1@gmx.at>
2024-09-11 15:17:40 +0200

    js filetypes

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -374,6 +374,7 @@ in {
           servers = {
 			tsserver = {
 				enable = true;
+				filetypes = [ "js" "mjs" "templ" "html" "ts" "tsx" "jsx" ];
 			};
             nixd.enable = true;
 			clangd.enable = true;
