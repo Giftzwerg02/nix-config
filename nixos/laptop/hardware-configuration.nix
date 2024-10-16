5f2bb299f759852610dc5795c4792196f812cf84
Benjamin Komar <chessplayer1@gmx.at>
2024-10-16 17:03:23 +0200

    laptop rebuild

--- nixos/laptop/hardware-configuration.nix
+++ nixos/laptop/hardware-configuration.nix
@@ -29,9 +29,9 @@
     fsType = "vfat";
   };
 
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/fd3cbe63-aa34-4266-be89-15dc043812eb";}
-  ];
+  # swapDevices = [
+  #   {device = "/dev/disk/by-uuid/fd3cbe63-aa34-4266-be89-15dc043812eb";}
+  # ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
