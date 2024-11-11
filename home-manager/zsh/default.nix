dc2624f356cdce8c08c555ef547cc6bc8b9236e1
Giftzwerg02 <chessplayer1@gmx.at>
2024-11-11 03:45:42 +0100

    update

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -41,30 +41,30 @@ in {
         bash
         */
         ''
-          function search_fzf() {
-            p=$(fd . . | fzf)
-            if [ -d "''${p}" ]; then
-          	  cd "''${p}"
-            else
-          	  cd "$(dirname "''${p}")"
-            fi
-          }
+               function search_fzf() {
+                 p=$(fd . . | fzf)
+                 if [ -d "''${p}" ]; then
+               	  cd "''${p}"
+                 else
+               	  cd "$(dirname "''${p}")"
+                 fi
+               }
 
-          zle -N search_fzf
+               zle -N search_fzf
 
-          bindkey '^f' search_fzf
+               bindkey '^f' search_fzf
 
-                 function update() {
-          local msg=''${1:-update}
+               function update() {
+               	local msg=''${1:-update}
 
-          cd ~/nix-config
-                 	alejandra ./
-          git add --all
-          git commit -m "''${msg}"
-          git push
-          sudo nixos-rebuild switch --flake .
+               	cd ~/nix-config
+                 alejandra ./
+           git add --all
+           git commit -m "''${msg}"
+           git push
+           sudo nixos-rebuild switch --flake .
           home-manager switch --flake .
-                 }
+               }
         '';
     };
   };
