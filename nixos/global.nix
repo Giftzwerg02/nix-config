123529ba1873a178aae75d6ae14bdf6f9ac16689
Giftzwerg02 <chessplayer1@gmx.at>
2024-11-20 00:46:32 +0100

    locale

--- nixos/global.nix
+++ nixos/global.nix
@@ -25,7 +25,7 @@
   # Select internationalisation properties.
   i18n = let
     # locale = "de_AT.UTF-8";
-    locale = "sk_SK.UTF-8";
+    locale = "slovak";
   in {
     defaultLocale = locale;
     extraLocaleSettings = {
