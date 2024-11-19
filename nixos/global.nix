62132538cb2c6317b93a7290ce0595f7687fe52d
Giftzwerg02 <chessplayer1@gmx.at>
2024-11-20 00:41:40 +0100

    locale

--- nixos/global.nix
+++ nixos/global.nix
@@ -23,18 +23,21 @@
   };
 
   # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "de_AT.UTF-8";
+  i18n = let
+    # locale = "de_AT.UTF-8";
+    locale = "sk_SK.UTF-8";
+  in {
+    defaultLocale = locale;
     extraLocaleSettings = {
-      LC_ADDRESS = "de_AT.UTF-8";
-      LC_IDENTIFICATION = "de_AT.UTF-8";
-      LC_MEASUREMENT = "de_AT.UTF-8";
-      LC_MONETARY = "de_AT.UTF-8";
-      LC_NAME = "de_AT.UTF-8";
-      LC_NUMERIC = "de_AT.UTF-8";
-      LC_PAPER = "de_AT.UTF-8";
-      LC_TELEPHONE = "de_AT.UTF-8";
-      LC_TIME = "de_AT.UTF-8";
+      LC_ADDRESS = locale;
+      LC_IDENTIFICATION = locale;
+      LC_MEASUREMENT = locale;
+      LC_MONETARY = locale;
+      LC_NAME = locale;
+      LC_NUMERIC = locale;
+      LC_PAPER = locale;
+      LC_TELEPHONE = locale;
+      LC_TIME = locale;
     };
   };
 
