ffa81484b781bd64b89efcf3ec4cedcfc0556777
Giftzwerg02 <chessplayer1@gmx.at>
2025-02-02 20:42:12 +0100

    sway (slay)

--- home-manager/sway/default.nix
+++ home-manager/sway/default.nix
@@ -179,57 +179,5 @@ in {
           # ];
         };
       };
-
-      programs.i3status-rust = {
-        enable = false;
-        bars = {
-          ${bar_name} = {
-            theme = "ctp-mocha";
-            icons = "awesome6";
-
-            blocks = [
-              {
-                block = "battery";
-              }
-
-              {
-                alert = 10.0;
-                block = "disk_space";
-                info_type = "available";
-                interval = 60;
-                path = "/";
-                warning = 20.0;
-              }
-
-              {
-                block = "memory";
-                format = " $icon $mem_used_percents ";
-                format_alt = " $icon $swap_used_percents ";
-              }
-
-              {
-                block = "cpu";
-                interval = 1;
-              }
-
-              {
-                block = "load";
-                format = " $icon $1m ";
-                interval = 1;
-              }
-
-              {
-                block = "sound";
-              }
-
-              {
-                block = "time";
-                format = " $timestamp.datetime(f:'%a %d/%m %R') ";
-                interval = 60;
-              }
-            ];
-          };
-        };
-      };
     };
 }
