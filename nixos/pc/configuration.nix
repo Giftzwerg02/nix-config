af9d11351cfa6c63b6b0ddd00f0368c7c7ad55f0
Giftzwerg02 <chessplayer1@gmx.at>
2025-05-03 18:00:40 +0200

    plasma6

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -96,6 +96,11 @@
   };
 
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    elisa
+  ];
 
   services.xserver = {
     displayManager.setupCommands = ''
