07e96199d069ad696aae37b26da02f97346eee03
Giftzwerg02 <chessplayer1@gmx.at>
2025-05-26 15:16:47 +0200

    mime firefox default

--- home-manager/mimeApps/default.nix
+++ home-manager/mimeApps/default.nix
@@ -21,6 +21,13 @@ in {
         "image/*" = ["feh.desktop"];
         "video/*" = ["mpv.desktop"];
         "inode/directory" = ["thunar.desktop"];
+
+        "default-web-browser" = ["firefox.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/about" = ["firefox.desktop"];
+        "x-scheme-handler/unknown" = ["firefox.desktop"];
       };
     };
   };
