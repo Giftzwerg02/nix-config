fb2daee9664950d1344d52e794129ca0fb98ef96
Giftzwerg02 <chessplayer1@gmx.at>
2025-05-26 17:31:09 +0200

    nushell

--- home-manager/nushell/default.nix
+++ home-manager/nushell/default.nix
@@ -11,8 +11,6 @@
       shellAliases = {
         hms = "nh home switch ~/nix-config";
         ncs = "nh os switch ~/nix-config";
-        switch = "ncs; hms";
-        cfg = "cd ~/nix-config; nvim .";
         ls = "eza";
         find = "fd";
         cat = "bat";
