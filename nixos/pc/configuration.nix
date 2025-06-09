80587166a37384ffe78e022403d62e76eb7e50b4
Giftzwerg02 <chessplayer1@gmx.at>
2025-06-09 02:43:48 +0200

    gamemode

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -268,6 +268,13 @@
 
   programs.gamemode = {
     enable = true;
+    settings = {
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    }
+    ;
   };
 
   programs.steam = {
