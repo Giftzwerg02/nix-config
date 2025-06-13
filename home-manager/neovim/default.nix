4a0a52fcbccaf1151ca005ce5e12ae3ca5bc70ae
Giftzwerg02 <chessplayer1@gmx.at>
2025-06-13 22:22:45 +0200

    pylsp

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -548,6 +548,12 @@ in {
                   isort.enabled = true;
                   pylint.enabled = true;
                   pylsp_mypy.enabled = true;
+                  jedi_hover.enabled = true;
+                  jedi_symbols.enabled = true;
+                  jedi_completion.enabled = true;
+                  jedi_definition.enabled = true;
+                  jedi_references.enabled = true;
+                  jedi_signature_help.enabled = true;
                 };
               };
               svelte.enable = true;
