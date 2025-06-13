64c776ef6f5a66627c4dafb8ed592ae495a84d8d
Giftzwerg02 <chessplayer1@gmx.at>
2025-06-13 22:26:29 +0200

    pylsp

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -536,24 +536,23 @@ in {
                 };
               };
               lua_ls.enable = true;
-              ruff.enable = true;
               pylsp = {
                 enable = true;
                 settings.plugins = {
-                  ruff.enabled = true;
-                  flake8 = {
-                    enabled = true;
-                    maxLineLength = 120;
-                  };
-                  isort.enabled = true;
-                  pylint.enabled = true;
-                  pylsp_mypy.enabled = true;
+                  # ruff.enabled = true;
+                  # flake8 = {
+                  #   enabled = true;
+                  #   maxLineLength = 120;
+                  # };
+                  # isort.enabled = true;
+                  # pylint.enabled = true;
+                  # pylsp_mypy.enabled = true;
                   jedi_hover.enabled = true;
-                  jedi_symbols.enabled = true;
+                  # jedi_symbols.enabled = true;
                   jedi_completion.enabled = true;
                   jedi_definition.enabled = true;
-                  jedi_references.enabled = true;
-                  jedi_signature_help.enabled = true;
+                  # jedi_references.enabled = true;
+                  # jedi_signature_help.enabled = true;
                 };
               };
               svelte.enable = true;
