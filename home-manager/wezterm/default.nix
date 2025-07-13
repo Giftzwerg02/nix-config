9bbad620c5ec8166026c74e2e711cf130d6dc114
Giftzwerg02 <chessplayer1@gmx.at>
2025-07-13 22:00:53 +0200

    wezterm + cleanup

--- home-manager/wezterm/default.nix
+++ home-manager/wezterm/default.nix
@@ -7,13 +7,19 @@
       local mux = wezterm.mux
       local config = {}
 
+      local function add_to_package_path(dir)
+          local path = package.path
+          package.path = dir .. '/?.lua;' .. path
+      end
+
       wezterm.on('gui-startup', function(cmd)
         local load_template = os.getenv('MY_WEZTERM_LOAD_LAYOUT')
         if not load_template then
           return
         end
 
-        local template = require('.layout')
+        add_to_package_path(load_template .. '/.layout')
+        local template = require('layout')
         template.apply(wezterm, config, cmd)
       end)
 
