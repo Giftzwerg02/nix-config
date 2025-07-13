97d770023a4a0f418acaf9921d8e5ce127f3b5b1
Giftzwerg02 <chessplayer1@gmx.at>
2025-07-13 21:07:53 +0200

    wezterm + cleanup

--- home-manager/wezterm/default.nix
+++ home-manager/wezterm/default.nix
@@ -3,6 +3,14 @@
 }: {
   programs.wezterm = {
     enable = true;
-    extraConfig = /* lua */ '''';
+    extraConfig = /* lua */ ''
+      local mux = wezterm.mux
+
+      wezterm.on('gui-startup', function(cmd)
+        local tab, pane, window = mux.spawn_window(cmd or {})
+        pane:split { size = 0.3 }
+        pane:split { size = 0.5 }
+      end)
+    '';
   };
 }
