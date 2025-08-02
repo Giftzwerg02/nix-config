7b8a3b4e1014985ef2def4b72195bb42a780568a
Giftzwerg02 <chessplayer1@gmx.at>
2025-08-02 17:51:30 +0200

    trying out cachyos kernel

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
   description = "My nixos config";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://chaotic-nyx.cachix.org/"
-    ];
-    extra-trusted-public-keys = [
-      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-    ];
-  };
-
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
