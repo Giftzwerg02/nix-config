97d286d340da4dee96748a1141d3f7f434176457
Giftzwerg02 <chessplayer1@gmx.at>
2025-12-10 00:19:12 +0100

    test march/tune

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -36,6 +36,12 @@
     config = {
       allowUnfree = true;
     };
+
+    hostPlatform = {
+      gcc.arch = "x86-64-v3";
+      gcc.tune = "x86-64-v3";
+      system = "x86_64-linux";
+    };
   };
 
   nix = {
