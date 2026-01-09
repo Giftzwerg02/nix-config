370284a7ee04a5f8a7a9e07e96870aa9c683f476
Giftzwerg02 <chessplayer1@gmx.at>
2026-01-09 14:40:19 +0100

    remove treesitter-textobjects

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -388,55 +388,6 @@ in {
               sync_install = false;
             };
           };
-          treesitter-textobjects = {
-            enable = true;
-            settings = {
-              select = {
-                enable = true;
-                lookahead = true;
-                keymaps = {
-                  "aa" = "@parameter.outer";
-                  "ia" = "@parameter.inner";
-                  "af" = "@function.outer";
-                  "if" = "@function.inner";
-                  "ac" = "@class.outer";
-                  "ic" = "@class.inner";
-                };
-              };
-               
-              move = {
-                enable = true;
-                goto_next_start = {
-                  "]m" = "@function.outer";
-                  "]]" = "@class.outer";
-                };
-
-                goto_next_end = {
-                  "]M" = "@function.outer";
-                  "][" = "@class.outer";
-                };
-
-                goto_previous_start = {
-                  "[m" = "@function.outer";
-                  "[[" = "@class.outer";
-                };
-
-                goto_previous_end = {
-                  "[M" = "@function.outer";
-                  "[]" = "@class.outer";
-                };
-              };
-              swap = {
-                enable = true;
-                swap_next = {
-                  "<leader>a" = "@parameter.inner";
-                };
-                swap_previous = {
-                  "<leader>A" = "@parameter.inner";
-                };
-              };
-            };
-          };
 
           conform-nvim = {
             settings = {
