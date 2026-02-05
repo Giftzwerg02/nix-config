ded67d420dd0ced48e1fcf80ddced685b5b96292
Giftzwerg02 <chessplayer1@gmx.at>
2026-02-06 00:34:05 +0100

    update

--- home-manager/firefox/default.nix
+++ home-manager/firefox/default.nix
@@ -17,22 +17,15 @@
 
     policies = {
       Preferences = let
-      ffVersion = config.programs.firefox.package.version;
-    in {
-      "media.ffmpeg.vaapi.enabled" = lib.versionOlder ffVersion "137.0.0";
-      "media.hardware-video-decoding.force-enabled" = lib.versionAtLeast ffVersion "137.0.0";
-      "media.rdd-ffmpeg.enabled" = lib.versionOlder ffVersion "97.0.0";
+        ffVersion = config.programs.firefox.package.version;
+      in {
+        "media.ffmpeg.vaapi.enabled" = lib.versionOlder ffVersion "137.0.0";
+        "media.hardware-video-decoding.force-enabled" = lib.versionAtLeast ffVersion "137.0.0";
+        "media.rdd-ffmpeg.enabled" = lib.versionOlder ffVersion "97.0.0";
 
-      "gfx.x11-egl.force-enabled" = true;
-      "widget.dmabuf.force-enabled" = true;
-
-      # Set this to true if your GPU supports AV1.
-      #
-      # This can be determined by reading the output of the
-      # `vainfo` command, after the driver is enabled with
-      # the environment variable.
-      "media.av1.enabled" = false;
-    };
+        "gfx.x11-egl.force-enabled" = true;
+        "widget.dmabuf.force-enabled" = true;
+      };
     };
   };
 }
