275632f93ea57c73205b9532e123a37061bd0df3
Giftzwerg02 <chessplayer1@gmx.at>
2026-03-29 15:15:38 +0200

    jupytext

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -423,7 +423,40 @@ in {
             ruff.enable = true;
             sqls.enable = true;
             ty.enable = true;
-            jupytext.enable = true;
+            jupytext = {
+              enable = false;
+              config = {
+                style = "hydrogen";
+                output_extension = "auto";
+                custom_language_formatting = {};
+              };
+            };
+            molten = {
+              enable = true;
+
+              # Configuration settings for molten.nvim. More examples at https://github.com/nix-community/nixvim/blob/main/plugins/by-name/molten/default.nix#L191
+              config = {
+                auto_image_popup = false;
+                auto_init_behavior = "init";
+                auto_open_html_in_browser = false;
+                auto_open_output = true;
+                cover_empty_lines = false;
+                copy_output = false;
+                enter_output_behavior = "open_then_enter";
+                image_provider = "none";
+                output_crop_border = true;
+                output_virt_lines = false;
+                output_win_border = [ "" "━" "" "" ];
+                output_win_hide_on_leave = true;
+                output_win_max_height = 15;
+                output_win_max_width = 80;
+                save_path.__raw = "vim.fn.stdpath('data')..'/molten'";
+                tick_rate = 500;
+                use_border_highlights = false;
+                limit_output_chars = 10000;
+                wrap_output = false;
+              };
+            };
             ts_ls = {
               enable = false;
               config = {
